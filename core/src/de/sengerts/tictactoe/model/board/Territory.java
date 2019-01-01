74afcc5b1cbd99615c04f1ca873f586509531da6
Tobias Senger <senger.tobias@web.de>
2019-01-01 16:26:26 +0100

    Fixed JavaDoc

--- core/src/de/sengerts/tictactoe/model/board/Territory.java
+++ core/src/de/sengerts/tictactoe/model/board/Territory.java
@@ -86,8 +86,7 @@ public class Territory {
 	 * Gets if a given row in this territory is completely marked by one player by
 	 * checking if all elements of the stream of all tiles in this territory
 	 * filtered by the given row are marked and all tiles have been marked by the
-	 * same player. {@link #isStreamOfTilesCompletelyMarkedByOnePlayer(HasStyle
-	 * tilesStreamSupplier)}
+	 * same player. {@link #isStreamOfTilesCompletelyMarkedByOnePlayer(HasStyle)}
 	 * 
 	 * @param row the row of the tiles to check
 	 * @return true if all tiles in the row are marked by the same player, otherwise
@@ -104,8 +103,7 @@ public class Territory {
 	 * Gets if a given column in this territory is completely marked by one player
 	 * by checking if all elements of the stream of all tiles in this territory
 	 * filtered by the given column are marked and all tiles have been marked by the
-	 * same player. {@link #isStreamOfTilesCompletelyMarkedByOnePlayer(HasStyle
-	 * tilesStreamSupplier)}
+	 * same player. {@link #isStreamOfTilesCompletelyMarkedByOnePlayer(HasStyle)}
 	 * 
 	 * @param column the column of the tiles to check
 	 * @return true if all tiles in the column are marked by the same player,
